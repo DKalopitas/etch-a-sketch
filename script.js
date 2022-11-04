caa4e0c13745aa1acd94d03d9d8c2b1845db6820
dimitris <kalopitas.dhmhtrhs@gmail.com>
2022-11-04 20:12:57 +0200

    Change color on mouseover for each grid-item and change color of grid-container

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ function createGrid() {
         for (let j = 0; j < 16; j++) {
             const div = document.createElement('div');
             div.classList.add('grid-item');
-            // div.textContent = `${i} - ${j}`;
             gridContainer.appendChild(div);
         }
     }
 }
 
 createGrid();
+
+const gridItems = document.querySelectorAll('.grid-item');
+gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', ()=> {
+    gridItem.classList.add('hovered');
+}))
