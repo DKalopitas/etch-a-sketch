59cec69f72b092700c8ae517c059f011cb7715db
dimitris <kalopitas.dhmhtrhs@gmail.com>
2022-11-05 00:43:20 +0200

    Make random color on grid-item hover and darken if hovered again

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function createGrid(columns = 16) {
         for (let j = 0; j < columns; j++) {
             const div = document.createElement('div');
             div.classList.add('grid-item');
+            div.setAttribute('id', `${i}-${j}`);
             gridContainer.appendChild(div);
         }
     }
@@ -30,18 +31,32 @@ function setGrid() {
     } else {
         alert('Invalid ammount!\nPlease enter a number between 0 and 100');
     }
-    const gridItems = document.querySelectorAll('.grid-item');
-    console.log(gridItems.length);
-    gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', ()=> {
-        gridItem.classList.add('hovered');
-    }))
+    changeColor();
+}
 
+function changeColor() {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', ()=> {
+        const id = gridItem.getAttribute('id');
+        const item = document.getElementById(id);
+        if (gridItem.getAttribute('class') === 'grid-item hovered') {
+            let rgb =  window.getComputedStyle(item).backgroundColor;
+            rgb = rgb.replace(/[^\d,]/g, '').split(',');
+            const r = Math.floor(rgb[0] * 0.8);
+            const g = Math.floor(rgb[1] * 0.8);
+            const b = Math.floor(rgb[2] * 0.8);
+            console.log(r);
+            document.getElementById(id).style.backgroundColor = 
+                `rgb(${r}, ${g}, ${b})`;
+        } else {
+            const r = Math.floor(Math.random() * (255 - 10 + 1) + 10);
+            const g = Math.floor(Math.random() * (255 - 10 + 1) + 10);
+            const b = Math.floor(Math.random() * (255 - 10 + 1) + 10);
+            item.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            item.classList.add('hovered');
+        }
+    }));
 }
 
 createGrid();
-
-const gridItems = document.querySelectorAll('.grid-item');
-console.log(gridItems.length);
-gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', ()=> {
-    gridItem.classList.add('hovered');
-}))
+changeColor();
