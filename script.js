a246a143d3161948ca22797bede38bdfd549d941
dimitris <kalopitas.dhmhtrhs@gmail.com>
2022-11-04 22:22:14 +0200

    Add button to change the grid ammount of divs/boxes specified by the user

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function createGrid() {
+function createGrid(columns = 16) {
     const gridContainer = document.querySelector('.grid-container');
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+    document.getElementById('temp').style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+    for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < columns; j++) {
             const div = document.createElement('div');
             div.classList.add('grid-item');
             gridContainer.appendChild(div);
@@ -9,9 +10,36 @@ function createGrid() {
     }
 }
 
+function removeGrid() {
+    const gridContainer = document.querySelector('.grid-container');
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(gridItem => gridContainer.removeChild(gridItem));
+}
+
+function setGrid() {
+    let columns = prompt('Enter ammount of boxes per side:');
+    if (columns === null) {
+        return;
+    }
+    columns = parseInt(columns);
+    if (columns >= 0 && columns <= 100) {
+        removeGrid();
+        createGrid(columns);
+    } else {
+        alert('Invalid ammount!\nPlease enter a number between 0 and 100');
+    }
+    const gridItems = document.querySelectorAll('.grid-item');
+    console.log(gridItems.length);
+    gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', ()=> {
+        gridItem.classList.add('hovered');
+    }))
+
+}
+
 createGrid();
 
 const gridItems = document.querySelectorAll('.grid-item');
+console.log(gridItems.length);
 gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', ()=> {
     gridItem.classList.add('hovered');
 }))
